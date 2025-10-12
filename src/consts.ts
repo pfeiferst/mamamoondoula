5a20d9577cb51b77da974d0b7614a4f50eb774ad
Stefan Pfeifer <pfeifer.stefan@gmail.com>
2025-10-12 14:17:52 +0200

    first creation with copy from existing page

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Mama-Moon-Doula';
+export const SITE_DESCRIPTION = 'Deine besondere Begleitung für Schwangerschaft - Geburt - Wochenbett in München und Umgebung';
