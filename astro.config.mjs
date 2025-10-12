41bde4a877d8b44ec15b090bbfc2969307470bdc
Martin Seibert <martin.seibert@seibert.group>
2025-10-12 16:24:56 +0200

    added form and logo

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://www.mama-moon-doula.de/',
+	output: 'server',
+	adapter: vercel(),
 	integrations: [mdx(), sitemap()],
 });
