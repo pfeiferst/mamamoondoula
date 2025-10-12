2d10072745abb3dd2fce808ed36718e40d6b1633
Martin Seibert <martin.seibert@seibert.group>
2025-10-12 17:04:47 +0200

    added canva layout for Anneli

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,16 @@ import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://www.mama-moon-doula.de/',
-	output: 'server',
-	adapter: vercel(),
-	integrations: [mdx(), sitemap()],
+  site: 'https://www.mama-moon-doula.de/',
+  output: 'server',
+  adapter: vercel(),
+  integrations: [mdx(), sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
